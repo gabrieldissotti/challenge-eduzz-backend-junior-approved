6b094de22ed0ad41735a979cd614e5a03f9e4acb
Gabriel Dissotti <gabrieldnrodrigues@gmail.com>
2020-03-08 23:33:15 -0300

    feat(quote): added method to get actual bitcoin quote

---
+++ src/app/controllers/QuoteController.ts
@@ -0,0 +1,72 @@
+import { Response } from 'express';
+import { Request } from '../middlewares/auth';
+import MercadoBitcoinApi from '../services/mercadobitcoin';
+
+class BalanceController {
+  /**
+   * @swagger
+   *
+   *
+   * /quotes:
+   *    get:
+   *      tags:
+   *        - Bitcoin Quote
+   *      name: Bitcoin Quote
+   *      summary: get bitcoin quote
+   *      security:
+   *        - bearerAuth: []
+   *      produces:
+   *        - application/json
+   *      consumes:
+   *        - application/json
+   *      responses:
+   *        200:
+   *          description: success
+   *          content:
+   *            application/json:
+   *              schema:
+   *                type: object
+   *                properties:
+   *                  bitcoin:
+   *                    type: number
+   *        401:
+   *          description: forbidden
+   *          content:
+   *            application/json:
+   *              schema:
+   *                type: object
+   *                properties:
+   *                  message:
+   *                    type: string
+   *                    example: 'Token not provided or inválid token'
+   *        500:
+   *          description: internal server error
+   *          content:
+   *            application/json:
+   *              schema:
+   *                type: object
+   *                properties:
+   *                  error:
+   *                    type: string
+   *                    example: 'Timeout'
+   */
+  async index (req: Request, res: Response): Promise<Response> {
+    try {
+      const { data } = await MercadoBitcoinApi.get('/BTC/ticker/')
+
+      const response = {
+        bitcoin: {
+          buy: data.ticker.buy,
+          sell: data.ticker.sell
+        }
+      }
+
+      return res.json(response)
+    } catch (error) {
+      console.log(error)
+      return res.status(500).json({ error: error.message });
+    }
+  }
+}
+
+export default new BalanceController();
--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,7 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import CreditController from './app/controllers/CreditController';
 import BalanceController from './app/controllers/BalanceController';
+import QuoteController from './app/controllers/QuoteController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -20,5 +21,7 @@ routes.use(authMiddleware);
 routes.post('/credits', CreditController.store);
 
 routes.get('/balances', BalanceController.index);
+
+routes.get('/quotes', QuoteController.index);
 
 export default routes;
